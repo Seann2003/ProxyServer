b4724c4368e739d7cfad61547b434b685d32d7a5
SohZHong <sohzhehong09@gmail.com>
2023-08-07 09:14:13 +0800

    Initial commit

---
+++ mindark-proxy/src/main/java/com/example/mindarkproxy/MindarkProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindarkproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindarkProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindarkProxyApplication.class, args);
+    }
+
+}
---
+++ mindark-proxy/src/test/java/com/example/mindarkproxy/MindarkProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindarkproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindarkProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
